aa987e9457dbd279a9a19cbff745ad7478001ea3
Michael St. Pierre <mikejstpierre@aol.com>
2025-06-08 18:20:26 -0400

    Add GET /shows route

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, jsonify
+from flask import Flask, request, jsonify
 from flask_cors import CORS
 import os
 import json
@@ -8,14 +8,20 @@ CORS(app)
 
 DATA_FILE = os.path.join(os.getcwd(), "latest_shows.json")
 
-@app.route("/shows")
-def get_shows():
-    if os.path.exists(DATA_FILE):
-        with open(DATA_FILE, "r", encoding="utf-8") as f:
-            data = json.load(f)
-            return jsonify(data.get("shows", []))
-    else:
-        return jsonify([])  # Empty if file doesn't exist yet
+@app.route("/shows", methods=["GET", "POST"])
+def shows():
+    if request.method == "POST":
+        data = request.get_json()
+        with open(DATA_FILE, "w", encoding="utf-8") as f:
+            json.dump(data, f, indent=2)
+        return jsonify({"status": "success"}), 200
+
+    elif request.method == "GET":
+        if os.path.exists(DATA_FILE):
+            with open(DATA_FILE, "r", encoding="utf-8") as f:
+                data = json.load(f)
+                return jsonify(data.get("shows", []))
+        return jsonify([])
 
 if __name__ == "__main__":
     app.run(debug=True)
